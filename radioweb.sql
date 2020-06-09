8e4cc196004214caf93cea0164c54edc57e745d7
cristianworth <cristianweissmantel@gmail.com>
2020-06-08 21:15:42 -0300

    Add main e musica.

---
+++ radioweb.sql
@@ -0,0 +1,5 @@
+CREATE SCHEMA `RadioWeb` ;
+CREATE TABLE `RadioWeb`.`Musica` (
+  `id_musica` INT NOT NULL AUTO_INCREMENT,
+  `nome_musica` VARCHAR(150) NULL,
+  PRIMARY KEY (`idMusica`));
---
+++ src/main/java/radioweb/serverController/DBConexao.java
@@ -0,0 +1,26 @@
+package radioweb.serverController;
+
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConexao {
+    private static final String URL = "jdbc:mysql://200.132.195.111/RadioWeb";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String USUARIO = "aluno";
+    private static final String SENHA = "aluno";
+    
+    public Statement getConnection() throws SQLException {
+        try {
+            Class.forName( DRIVER );
+            
+            Connection conn = DriverManager.getConnection(URL, USUARIO, SENHA);
+            return conn.createStatement();
+        }
+        catch (ClassNotFoundException e) {
+            throw new SQLException(e.getMessage());
+        }
+    }
+}
