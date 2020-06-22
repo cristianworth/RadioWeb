35a56a372bcabb3d48516b962b946b65c256cef1
cristianworth <cristianweissmantel@gmail.com>
2020-06-22 20:25:42 -0300

    Converter MusicProtocol e JSON.

--- src/main/java/Server/MusicProtocol.java
+++ src/main/java/Server/MusicProtocol.java
@@ -5,19 +5,42 @@
  */
 package Server;
 
+import java.io.IOException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.ObjectWriter;
+
 /**
  *
  * @author Marcelo
  */
 public class MusicProtocol {
+
     private int index;
     private String nome;
     private byte[] stream;
+    private int total;
 
-    public MusicProtocol(int index, String nome, byte[] stream) {
+    public MusicProtocol() {
+
+    }
+
+    public MusicProtocol(int index, String nome, byte[] stream, int total) {
         this.index = index;
         this.nome = nome;
         this.stream = stream;
+        this.total = total;
+    }
+
+    public MusicProtocol getProtocolo(String json) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        MusicProtocol protocolo = mapper.readValue(json, MusicProtocol.class);  
+        return protocolo;
+    }
+
+    public String getJson() throws IOException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String JsonProcoloMusica = ow.writeValueAsString(this);
+        return JsonProcoloMusica;
     }
 
     public int getIndex() {
